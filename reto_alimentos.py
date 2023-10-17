b844ec45d69780c2e7569a00ea5eb8a8f587c215
holacomoestas22 <145036535+holacomoestas22@users.noreply.github.com>
2023-10-17 08:54:13 -0600

    Create reto_alimentos.py
    Claudia Estefania Gutierrez Guzman

---
+++ reto_alimentos.py
@@ -0,0 +1,16 @@
+https://replit.com/join/upxzcaigjx-claudia-estefa1
+grasa = float(input("Dime la grasa en gramos (recuerda que es por porcion)"))
+print("")
+azucar = float(input("Dame la azucar en gramos (recuerda que es por porcion)"))
+print("")
+sodio = float(input("dame el sodio en mg (recuerda que es por porcion) "))
+print("")
+
+if grasa <= 5 and azucar <= 10 :
+  print("el alimento es bajo en grasa y azucar :) ")
+
+elif sodio <=100 :
+  print("El alimento es bajo en sodio :)")
+
+else :
+  print("Ten en consideracion las advertencias nutricionales, cuidate!")
